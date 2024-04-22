6dbb80f24bb8530955e367a0c30cbae68b1c7283
111DataScienceWizard <164731788+111DataScienceWizard@users.noreply.github.com>
2024-04-22 15:28:11 +0530

    Update main1.py

--- Web_App_Trebirth/main1.py
+++ Web_App_Trebirth/main1.py
@@ -1,19 +1,11 @@
 import streamlit as st
 import numpy as np
-import google.cloud
 from google.cloud import firestore
 import pandas as pd
-import joblib
-from prediction import predict
-#from google.cloud import FieldFilter
 from google.cloud.firestore import FieldFilter
-#from google.cloud.firestore.types import FieldFilter
-from sklearn.metrics import accuracy_score, classification_report
-from scipy import signal
-from sklearn.preprocessing import MinMaxScaler
 from io import BytesIO
 
-
+# Set page configuration
 st.set_page_config(layout="wide")
 st.title('Data Analytics')
 st.markdown(
@@ -25,18 +17,15 @@ st.markdown(
     </style>
     """,
     unsafe_allow_html=True,
-  )
+)
 
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("Web_App_Trebirth/testdata1-20ec5-firebase-adminsdk-an9r6-a87cacba1d.json")
 
 number_row = st.text_input('Enter Row number', '1')
 number = st.text_input('Enter Tree number', '1')
- 
+
 # Create a reference to the Google post.
-#doc_ref = db.collection("T1R1").select("ADXL Raw", "Radar Raw").stream()
-
-
 query = db.collection('DevOps').where(filter=FieldFilter("RowNo", "==", int(number_row))).where(filter=FieldFilter("TreeNo", "==", int(number))).get()
 
 radar_data = []
@@ -59,22 +48,22 @@ df_combined = df_combined[100:1800]
 st.write(df_combined)
 
 @st.cache
-def convert_df(df):
+def convert_df_to_csv(df):
     return df.to_csv().encode('utf-8')
 
-csv_combined = convert_df(df_combined)
+csv_combined = convert_df_to_csv(df_combined)
 
 st.download_button("Download Combined Radar and ADXL Data", csv_combined, "Combined_Data.csv", "text/csv", key='download-csvcombined')
 
 
-
+# Metadata extraction
 df_metadata = pd.DataFrame(columns=['Key', 'Value'])
 
 # Iterate over documents and extract metadata
 for doc in query:
     metadata = doc.to_dict()
     for key, value in metadata.items():
-        df_metadata = df_metadata.append({'Key': key, 'Value': value}, ignore_index=True)
+        df_metadata = df_metadata.append({'Key': key, 'Value': str(value)}, ignore_index=True)
 
 # Convert DataFrame to Excel format
 excel_data = BytesIO()
