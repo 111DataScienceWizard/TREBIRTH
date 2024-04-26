071a4a15980a7557896ac26af706b3cec4c54fea
111DataScienceWizard <164731788+111DataScienceWizard@users.noreply.github.com>
2024-04-26 18:39:20 +0530

    Update main2.py

--- Web_App_Trebirth/main2.py
+++ Web_App_Trebirth/main2.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from google.cloud import firestore
 import pandas as pd
-from google.cloud.firestore_v1 import FieldPath
-from io import BytesIO  # Import BytesIO class from io module
 import xlsxwriter
 from datetime import datetime
 
@@ -34,7 +32,7 @@ query = db.collection('DevOps')
 
 # Filter based on user input
 if row_number != 'All':
-    query = query.where(FieldPath.document_id(), '==', row_number)
+    query = query.where('RowNo', '==', int(row_number))
 if tree_number != 'All':
     query = query.where('TreeNo', '==', int(tree_number))
 if scan_number != 'All':
