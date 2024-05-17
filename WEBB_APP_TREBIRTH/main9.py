c6c7b41f1e2e0ff1ec0ca23d38aa60fbde6dce09
111DataScienceWizard <164731788+111DataScienceWizard@users.noreply.github.com>
2024-05-17 17:25:16 +0530

    Update main9.py

--- WEBB_APP_TREBIRTH/main9.py
+++ WEBB_APP_TREBIRTH/main9.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import numpy as np
 from scipy import signal
 from scipy.stats import skew, kurtosis
+from preprocess import detrend, fq, stats_radar, columns_reports_unique
 from Filters import (coefLPF1Hz, coefLPF2Hz, coefLPF3Hz, coefLPF4Hz, coefLPF5Hz, coefLPF6Hz, coefLPF7Hz, coefLPF8Hz, 
                      coefLPF9Hz, coefLPF10Hz, coefLPF11Hz, coefLPF12Hz, coefLPF13Hz, coefLPF14Hz, coefLPF15Hz, 
                      coefLPF16Hz, coefLPF17Hz, coefLPF18Hz, coefLPF19Hz, coefLPF20Hz, coefLPF21Hz, coefLPF22Hz, 
@@ -22,75 +23,6 @@ from Filters import (coefLPF1Hz, coefLPF2Hz, coefLPF3Hz, coefLPF4Hz, coefLPF5Hz,
                      coefHPF37Hz, coefHPF38Hz, coefHPF39Hz, coefHPF40Hz, coefHPF41Hz, coefHPF42Hz, coefHPF43Hz, 
                      coefHPF44Hz, coefHPF45Hz, coefHPF46Hz, coefHPF47Hz, coefHPF48Hz, coefHPF49Hz, coefHPF50Hz)
 
-# Define detrend function
-def detrend(dataframe):
-    detrended_data = dataframe - dataframe.mean()
-    return detrended_data
-
-# Define feature extraction functions
-def fq(df):
-    frequencies = []
-    powers = []
-
-    for i in df:
-        f, p = signal.welch(df[i], 100, 'flattop', 1024, scaling='spectrum')
-        frequencies.append(f)
-        powers.append(p)
-
-    frequencies = pd.DataFrame(frequencies)
-    powers = pd.DataFrame(powers)
-    return frequencies, powers
-
-def stats_radar(df):
-    result_df = pd.DataFrame()
-
-    for column in df.columns:
-        std_list, ptp_list, mean_list, rms_list = [], [], [], []
-
-        std_value = np.std(df[column])
-        ptp_value = np.ptp(df[column])
-        mean_value = np.mean(df[column])
-        rms_value = np.sqrt(np.mean(df[column]**2))
-
-        std_list.append(std_value)
-        ptp_list.append(ptp_value)
-        mean_list.append(mean_value)
-        rms_list.append(rms_value)
-
-        column_result_df = pd.DataFrame({
-            "STD": std_list,
-            "PTP": ptp_value,
-            "Mean": mean_list,
-            "RMS": rms_list
-        })
-        result_df = pd.concat([result_df, column_result_df], axis=0)
-    return result_df
-
-# Define function to compare columns
-def columns_reports_unique(df):
-    report = []
-    num_columns = len(df.columns)
-    for i in range(num_columns):
-        for j in range(i + 1, num_columns):  # Start j from i + 1
-            column1 = df.columns[i]
-            column2 = df.columns[j]
-            diff = df[column1] - df[column2]
-            mean_diff = np.mean(diff)
-            deviation_diff = np.std(diff)
-            ptp_diff = np.ptp(diff)
-            skewness_diff = skew(diff)
-            correlation = df[[column1, column2]].corr().iloc[0, 1]
-            report.append({
-                'Column 1': column1,
-                'Column 2': column2,
-                'Mean Difference': mean_diff,
-                'Deviation Difference': deviation_diff,
-                'PTP Difference': ptp_diff,
-                'Skewness Difference': skewness_diff,
-                'Correlation': correlation,
-            })
-    report_df = pd.DataFrame(report)
-    return report_df
 
 # Filter processing function
 def process(coef, in_signal):
