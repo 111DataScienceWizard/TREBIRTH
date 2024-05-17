efe7f9cb189d77ab1ec72c71b420a4815ae3eed3
111DataScienceWizard <164731788+111DataScienceWizard@users.noreply.github.com>
2024-05-17 13:27:46 +0530

    Update main4.py

--- WEBB_APP_TREBIRTH/main4.py
+++ WEBB_APP_TREBIRTH/main4.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import numpy as np
 from scipy import signal
 from scipy.stats import skew, kurtosis
-from FILTERS import  coefLPF1Hz, coefLPF2Hz, coefLPF3Hz, coefLPF4Hz, coefLPF5Hz, coefLPF6Hz, coefLPF7Hz, coefLPF8Hz, 
+from Filters import  coefLPF1Hz, coefLPF2Hz, coefLPF3Hz, coefLPF4Hz, coefLPF5Hz, coefLPF6Hz, coefLPF7Hz, coefLPF8Hz, 
                      coefLPF9Hz, coefLPF10Hz, coefLPF11Hz, coefLPF12Hz, coefLPF13Hz, coefLPF14Hz, coefLPF15Hz, 
                      coefLPF16Hz, coefLPF17Hz, coefLPF18Hz, coefLPF19Hz, coefLPF20Hz, coefLPF21Hz, coefLPF22Hz, 
                      coefLPF23Hz, coefLPF24Hz, coefLPF25Hz, coefLPF26Hz, coefLPF27Hz, coefLPF28Hz, coefLPF29Hz, 
