eb55d2f5772c4e5a25490fb30e6b63f1d9cc11f3
111DataScienceWizard <164731788+111DataScienceWizard@users.noreply.github.com>
2024-06-20 17:18:04 +0530

    Update 99.py

--- WEBB_APP_TREBIRTH/99.py
+++ WEBB_APP_TREBIRTH/99.py
@@ -195,10 +195,10 @@ else:
     desired_columns = ['DeviceName:', 'TreeSec', 'TreeNo', 'InfStat', 'TreeID', 'RowNo', 'ScanNo', 'timestamp']
     df_metadata_filtered = df_metadata[desired_columns]
 
-    # Convert index data to DataFrame if present
+    """# Convert index data to DataFrame if present
     if index_data:
         df_index = pd.DataFrame(index_data)
-        df_index_long = df_index.apply(pd.Series.explode)
+        df_index_long = df_index.apply(pd.Series.explode)"""
 
     # Function to debug and handle duplicate index issue
     def explode_and_handle_duplicates(df):
