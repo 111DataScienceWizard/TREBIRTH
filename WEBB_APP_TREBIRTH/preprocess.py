66d144b5ebceb633f7450090c83ba4e2bd5f4985
111DataScienceWizard <164731788+111DataScienceWizard@users.noreply.github.com>
2024-07-04 13:39:13 +0530

    Update preprocess.py

--- WEBB_APP_TREBIRTH/preprocess.py
+++ WEBB_APP_TREBIRTH/preprocess.py
@@ -19,7 +19,7 @@ def fq(df):
         powers.append(p[1:])
 
     frequencies_df = pd.DataFrame(frequencies).transpose()
-    powers_df = powers.transpose()
+    powers_df = pd.DataFrame(powers).transpose()
     return frequencies_df, powers_df
 
 def stats_radar(df):
