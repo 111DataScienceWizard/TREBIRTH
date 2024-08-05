e04024a2047f61a7269645008f07dd0bbb9daeba
111DataScienceWizard <164731788+111DataScienceWizard@users.noreply.github.com>
2024-08-05 18:48:26 +0530

    Update 555.py

--- WEBB_APP_TREBIRTH/555.py
+++ WEBB_APP_TREBIRTH/555.py
@@ -193,7 +193,8 @@ else:
     df_metadata = pd.DataFrame(metadata_list)
 
     # Select only the desired columns
-    desired_columns = ['DeviceName:', 'TreeSec', 'TreeNo', 'InfStat', 'TreeID', 'RowNo', 'ScanNo', 'timestamp']
+    #desired_columns = ['DeviceName:', 'TreeSec', 'TreeNo', 'InfStat', 'TreeID', 'RowNo', 'ScanNo', 'timestamp']
+    desired_columns = ['TreeSec', 'TreeNo', 'InfStat', 'TreeID', 'RowNo', 'ScanNo', 'timestamp']
     df_metadata_filtered = df_metadata[desired_columns]
 
     #Convert index data to DataFrame if present
