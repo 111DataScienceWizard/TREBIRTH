74a31fbdc40c0c306b667a429a50d2b16ee3a7f1
111DataScienceWizard <164731788+111DataScienceWizard@users.noreply.github.com>
2024-09-05 02:13:59 +0530

    Update trebirth.py

--- WEBB_APP_TREBIRTH/trebirth.py
+++ WEBB_APP_TREBIRTH/trebirth.py
@@ -14,7 +14,34 @@ from scipy import signal
 from scipy.stats import skew, kurtosis
 from preprocess import detrend, fq, calculate_statistics, columns_reports_unique, stats_filtereddata
 from google.api_core.exceptions import ResourceExhausted, RetryError
+from Filter import (coefLPF1Hz, coefLPF2Hz, coefLPF3Hz, coefLPF4Hz, coefLPF5Hz, coefLPF6Hz, coefLPF7Hz, coefLPF8Hz, 
+                     coefLPF9Hz, coefLPF10Hz, coefLPF11Hz, coefLPF12Hz, coefLPF13Hz, coefLPF14Hz, coefLPF15Hz, 
+                     coefLPF16Hz, coefLPF17Hz, coefLPF18Hz, coefLPF19Hz, coefLPF20Hz, coefLPF21Hz, coefLPF22Hz, 
+                     coefLPF23Hz, coefLPF24Hz, coefLPF25Hz, coefLPF26Hz, coefLPF27Hz, coefLPF28Hz, coefLPF29Hz, 
+                     coefLPF30Hz, coefLPF31Hz, coefLPF32Hz, coefLPF33Hz, coefLPF34Hz, coefLPF35Hz, coefLPF36Hz, 
+                     coefLPF37Hz, coefLPF38Hz, coefLPF39Hz, coefLPF40Hz, coefLPF41Hz, coefLPF42Hz, coefLPF43Hz, 
+                     coefLPF44Hz, coefLPF45Hz, coefLPF46Hz, coefLPF47Hz, coefLPF48Hz, coefLPF49Hz, coefLPF50Hz,
+                     coefHPF1Hz, coefHPF2Hz, coefHPF3Hz, coefHPF4Hz, coefHPF5Hz, coefHPF6Hz, coefHPF7Hz, coefHPF8Hz, 
+                     coefHPF9Hz, coefHPF10Hz, coefHPF11Hz, coefHPF12Hz, coefHPF13Hz, coefHPF14Hz, coefHPF15Hz, 
+                     coefHPF16Hz, coefHPF17Hz, coefHPF18Hz, coefHPF19Hz, coefHPF20Hz, coefHPF21Hz, coefHPF22Hz, 
+                     coefHPF23Hz, coefHPF24Hz, coefHPF25Hz, coefHPF26Hz, coefHPF27Hz, coefHPF28Hz, coefHPF29Hz, 
+                     coefHPF30Hz, coefHPF31Hz, coefHPF32Hz, coefHPF33Hz, coefHPF34Hz, coefHPF35Hz, coefHPF36Hz, 
+                     coefHPF37Hz, coefHPF38Hz, coefHPF39Hz, coefHPF40Hz, coefHPF41Hz, coefHPF42Hz, coefHPF43Hz, 
+                     coefHPF44Hz, coefHPF45Hz, coefHPF46Hz, coefHPF47Hz, coefHPF48Hz, coefHPF49Hz, coefHPF50Hz)
 
+def process(coef, in_signal):
+    FILTERTAPS = len(coef)
+    values = np.zeros(FILTERTAPS)
+    out_signal = []
+    gain = 1.0
+    k = 0
+    for in_value in in_signal:
+        values[k] = in_value
+        out = np.dot(coef, np.roll(values, k))
+        out /= gain
+        out_signal.append(out)
+        k = (k + 1) % FILTERTAPS
+    return out_signal
 
 # Set page configuration
 st.set_page_config(layout="wide")
@@ -69,10 +96,10 @@ bucket_number = st.text_input('Enter Bucket number', 'All')
 label_infstat = st.selectbox('Select Label', ['All', 'Infected', 'Healthy'], index=0)
 
 # Dropdown for selecting sheets in Excel
-selected_sheets = st.multiselect('Select Sheets', ['Raw Data', 'Index Data', 'Detrended Data', 'Normalized Data', 'Detrended & Normalized Data', 'Metadata', 'Time Domain Features', 'Frequency Domain Features', 'Frequency Domain Stats', 'Columns Comparison'], default=['Raw Data', 'Metadata'])
+selected_sheets = st.multiselect('Select Sheets', ['Raw Data', 'Detrended Data', 'Normalized Data', 'Detrended & Normalized Data', 'Metadata', 'Time Domain Features', 'Frequency Domain Features', 'Frequency Domain Stats', 'Columns Comparison'], default=['Raw Data', 'Metadata'])
 
 # Create a reference to the Firestore collection
-query = db.collection('demo_db')
+query = db.collection('debugging')
 
 # Apply filters based on user input
 if row_number != 'All':
@@ -103,7 +130,6 @@ else:
     ay_data = []
     az_data = []
     metadata_list = []
-    index_data = []
 
     # Function to slice data
     def slice_data(data):
@@ -124,8 +150,7 @@ else:
                 metadata[key] = value.replace(tzinfo=None)
         metadata_list.append(metadata)
 
-    # Function to process data and check for missing packets
-    # Function to process data without checking for missing packets
+    # Function to process data
     def process_data(data_list, prefix):
         processed_list = []
         for i, data in enumerate(data_list):
@@ -134,13 +159,13 @@ else:
             new_columns = [f'{prefix}{i+1}']
             df.columns = new_columns
             processed_list.append(df)
-        return pd.concat(processed_list, axis=1) if processed_list else pd.DataFrame()
+        return pd.concat(processed_list, axis=1)
 
-    df_radar = process_and_check_data(radar_data, [idx['IndexRadar'] for idx in index_data], 'Radar ')
-    df_adxl = process_and_check_data(adxl_data, [idx['IndexAx'] for idx in index_data], 'ADXL ')
-    df_ax = process_and_check_data(ax_data, [idx['IndexAx'] for idx in index_data], 'Ax ')
-    df_ay = process_and_check_data(ay_data, [idx['IndexAy'] for idx in index_data], 'Ay ')
-    df_az = process_and_check_data(az_data, [idx['IndexAz'] for idx in index_data], 'Az ')
+    df_radar = process_data(radar_data, 'Radar ')
+    df_adxl = process_data(adxl_data, 'ADXL ')
+    df_ax = process_data(ax_data, 'Ax ')
+    df_ay = process_data(ay_data, 'Ay ')
+    df_az = process_data(az_data, 'Az ')
 
     # Concatenate all DataFrames column-wise
     df_combined = pd.concat([df_radar, df_adxl, df_ax, df_ay, df_az], axis=1)
@@ -151,9 +176,10 @@ else:
     # Normalize all the columns
     df_combined_normalized = (df_combined_detrended - df_combined_detrended.min()) / (df_combined_detrended.max() - df_combined_detrended.min())
 
-    # Convert list of dictionaries to DataFrame
+    # Convert list of dictionaries to DataFrame for metadata
     df_metadata = pd.DataFrame(metadata_list)
-
+  
+    
     # Select only the desired columns
     desired_columns = ['DeviceName:', 'TreeSec', 'TreeNo', 'InfStat', 'TreeID', 'RowNo', 'ScanNo', 'timestamp']
     df_metadata_filtered = df_metadata[desired_columns]
