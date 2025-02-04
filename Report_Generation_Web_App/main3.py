a874d4fda1863c64f6c5d87976e67dd7e7bafaed
111DataScienceWizard <164731788+111DataScienceWizard@users.noreply.github.com>
2025-02-05 03:50:05 +0530

    Update main3.py

--- Report_Generation_Web_App/main3.py
+++ Report_Generation_Web_App/main3.py
@@ -181,13 +181,13 @@ def generate_pdf():
     
     desc = """This Trebirth test report is a supplementary report only, which""" 
     elements.append(Paragraph(desc, body_style))  # Add each line as a new paragraph
-    elements.append(Spacer(1, 16))
+    elements.append(Spacer(1, 10))
     desc1 = """MUST be read in conjunction with the full timber pest report.""" 
     elements.append(Paragraph(desc1, body_style))  # Add each line as a new paragraph
-    elements.append(Spacer(1, 16))
+    elements.append(Spacer(1, 10))
     desc2 = """This report cannot be relied upon without the full timber pest"""
     elements.append(Paragraph(desc2, body_style))  # Add each line as a new paragraph
-    elements.append(Spacer(1, 16))
+    elements.append(Spacer(1, 10))
     desc3 = """report and isonly a record of the test findings."""
     elements.append(Paragraph(desc3, body_style))  # Add each line as a new paragraph
     elements.append(Spacer(1, 16))  # Leave space between lines
