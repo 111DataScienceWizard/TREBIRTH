aa0c01bae6c45b60f92fe1728be1a38b46156c07
111DataScienceWizard <164731788+111DataScienceWizard@users.noreply.github.com>
2025-02-05 05:31:25 +0530

    Update main3.py

--- Report_Generation_Web_App/main3.py
+++ Report_Generation_Web_App/main3.py
@@ -178,10 +178,10 @@ def generate_pdf():
     
     elements = []
     elements.append(Paragraph("TERMATRAC TEST REPORT", heading_style_centered))
-    elements.append(Paragraph("SUPPLEMENT TO TIMBER PEST REPORT", heading_style_centered))
+    #elements.append(Paragraph("SUPPLEMENT TO TIMBER PEST REPORT", heading_style_centered))
     elements.append(Spacer(1, 16))
     
-    desc = """This Trebirth test report is a supplementary report only, which""" 
+    desc = """This Trebirth test report is a supplementary report only, which MUST be read in conjunction with the full timber pest report. This report cannot be relied upon without the full timber pest report and isonly a record of the test findings.""" 
     elements.append(Paragraph(desc, body_style))  # Add each line as a new paragraph
     elements.append(Spacer(1, 10))
     desc1 = """MUST be read in conjunction with the full timber pest report.""" 
