36e4a3f3d2dc851ba5d12e9a7b8204a960bf61a8
111DataScienceWizard <164731788+111DataScienceWizard@users.noreply.github.com>
2025-02-08 01:15:00 +0530

    Update main5.py

--- Report_Generation_Web_App/pages/main5.py
+++ Report_Generation_Web_App/pages/main5.py
@@ -224,12 +224,12 @@ def generate_pdf():
         report_date = filtered_scans[0]["scan_date"]
         
         # Split the general information into multiple lines and add a Spacer after each line
-        general_info = f"""
-        Tests were carried out by:         {test_by}  
-        Date:                                         {report_date}  
-        Report for building at:              {report_loc}  
-        Report requested by:                {requested_by}  
-        """
+        general_info = [
+            f"<b>Tests were carried out by:</b> {test_by}",
+            f"<b>Date:</b> {report_date}",
+            f"<b>Report for building at:</b> {report_loc}",
+            f"<b>Report requested by:</b> {requested_by}"
+        ]
         for info in general_info:
             elements.append(Paragraph(info, bold_style))
             elements.append(Spacer(1, 20))  # Leave space between lines
